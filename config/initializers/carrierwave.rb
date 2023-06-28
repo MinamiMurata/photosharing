1e1227cb519fbe5ac937fc07dc404bb5aad1bbc0
minami murata <minami_murata@outlook.com>
2023-06-28 18:26:36 +0900

    fixed carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -13,9 +13,9 @@ CarrierWave.configure do |config|
   case Rails.env
   when "production"
     config.fog_directory = "m-app-production"
-    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/本番環境用のバケット名"
+    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/m-app-production"
   when "development"
     config.fog_directory = "m-app-development"
-    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/開発環境用のバケット名"
+    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/m-app-development"
   end
 end
